8b0b7e7168677e2c11e7aedf389220b4febdacaf
Steven Whitfield <stevmwhitfield@gmail.com>
2024-08-29 14:06:51 -0500

    fix starter tests

--- src/app/category/category.spec.ts
+++ src/app/category/category.spec.ts
@@ -1,7 +1,9 @@
 import { Category } from './category';
 
 describe('Category', () => {
-  it('should create an instance', () => {
-    expect(new Category()).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        expect(
+            new Category(1, 'Category 1', 'Category 1 description')
+        ).toBeTruthy();
+    });
 });
