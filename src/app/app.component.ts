4db8e84b42c9649ffc0bf1031cd22785e4c82ba2
Steven Whitfield <stevmwhitfield@gmail.com>
2024-08-29 14:01:03 -0500

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, RouterLink],
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'final-project';
+    title = 'Angular Shop';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CategoryItemComponent } from './category-item/category-item.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'shop', component: CategoryItemComponent },
+];
